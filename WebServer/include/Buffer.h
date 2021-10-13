3c48081b3fbaa49fb3b1200fc2db1e49dc8cb0dd
xieyongqi <657023321@qq.com>
2021-10-13 18:13:03 +0800

    summary:     1??

--- WebServer/include/Buffer.h
+++ WebServer/include/Buffer.h
@@ -6,13 +6,38 @@
 template<size_t size>
 class Buffer {
 public:
-  Buffer() {
-    memset(buff_, 0, sizeof(buff_));
-    size_ = 0;
-    end_ = size + 1;
-    cap_ = size;
+  explicit Buffer()
+    : buff_(new char[size]),
+      size_(0),
+      cap_(size),
+      end_(size + 1) {
+    memset(buff_, 0, sizeof(cap_));
   }
-  ~Buffer() = default;
+
+  ~Buffer() {
+    if (buff_) {
+      delete[] buff_;
+    }
+  }
+
+  Buffer(const Buffer& a) {
+    construct(a);
+  }
+
+  Buffer& operator = (const Buffer& a) {
+    construct(a);
+    return *this;
+  }
+
+  Buffer(Buffer&& a) {
+    remove(std::move(a));
+  }
+
+  Buffer& operator = (Buffer&& a) {
+    remove(std::move(a));
+    return *this;
+  }
+
   size_t find(char t, int begin = 0) {
     if (begin > size_) {
       return end_;
@@ -44,9 +69,9 @@ public:
   inline void clear() {
     buff_[0] = '\0';
     size_ = 0;
+    //No necessary
 #ifdef DEBUG
-    //·Ç±ØÒª
-    memset(buff_, 0, sizeof(buff_));
+    memset(buff_, 0, cap_);
 #endif
   }
 
@@ -61,9 +86,9 @@ public:
   inline bool empty() {
     return size_ == 0;
   }
-
+  
   inline void setSize(size_t s) {
-    size_ = s;
+      size_ = s;
   }
 
   std::string substr(size_t begin, size_t len = 0) {
@@ -74,17 +99,38 @@ public:
       return std::string(&buff_[begin],
         begin + len >= size_ ?
         size_ - begin + 1 : len);
-    }
-    else {
+    } else {
       return std::string(&buff_[begin],
         size_ - begin + 1);
     }
   }
+  
+private:
+  void construct(const Buffer& a) {
+    buff_ = new char[a.cap_];
+    cap_ = a.cap_;
+    end_ = a.end_;
+    size_ = a.size_;
+    memcpy(buff_, a.buff_, size_);
+  }
+
+  void remove(Buffer&& a) {
+    buff_ = a.buff_;
+    size_ = a.size_;
+    cap_ = a.cap_;
+    end_ = a.end_;
+    a.reset();
+  }
+  void reset() {
+    buff_ = nullptr;
+    size_ = 0;
+    cap_ = 0;
+    end_ = 0;
+  }
 
 private:
-  char buff_[size];
+  char *buff_;
   size_t size_;
   size_t cap_;
   size_t end_;
 };
-
